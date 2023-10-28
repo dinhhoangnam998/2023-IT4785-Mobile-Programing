eba2d3b26415bdda52455fb49b060c926edb87e0
HoangNam <dinhhoangnam998@gmail.com>
2023-10-28 21:42:47 +0700

    :pencil2: refactor initialize app

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'package:flutter_2023_it4785/pages/map_gps/map_gps_page.dart';
 import 'package:flutter_2023_it4785/pages/map_tel/map_tel_page.dart';
 import 'package:flutter_2023_it4785/pages/settings/settings_page.dart';
 import 'package:flutter_2023_it4785/pages/telephony/telephony_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 class App extends StatefulWidget {
@@ -40,30 +41,43 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Find My Location'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        // backgroundColor: Colors.black,
-        // selectedItemColor: Colors.greenAccent,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.map), label: "Map Tel"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.gps_fixed), label: "Map GPS"),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: "Cell Towers"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: "Settings"),
-          // BottomNavigationBarItem(icon: Icon(Icons.phone), label: "Telephony"),
-        ],
-        currentIndex: _currentIndex,
-        onTap: (value) {
-          updateCurrentIndex(value);
-        },
-      ),
-      body: _pages[_currentIndex],
-    );
+    return BlocBuilder<CellTowersCubit, CellTowersState>(
+        builder: (context, state) {
+      return state.when(
+          initial: () => const Center(
+                // child: Text("Initializing App"),
+                child: CircularProgressIndicator(),
+              ),
+          error: (message) => const Center(
+                child: Text("Fail to initialize App"),
+              ),
+          loaded: (_) => Scaffold(
+                appBar: AppBar(
+                  title: const Text('Find My Location'),
+                ),
+                bottomNavigationBar: BottomNavigationBar(
+                  type: BottomNavigationBarType.fixed,
+                  // backgroundColor: Colors.black,
+                  // selectedItemColor: Colors.greenAccent,
+                  unselectedItemColor: Colors.grey,
+                  items: const [
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.map), label: "Map Tel"),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.gps_fixed), label: "Map GPS"),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.list), label: "Cell Towers"),
+                    BottomNavigationBarItem(
+                        icon: Icon(Icons.settings), label: "Settings"),
+                    // BottomNavigationBarItem(icon: Icon(Icons.phone), label: "Telephony"),
+                  ],
+                  currentIndex: _currentIndex,
+                  onTap: (value) {
+                    updateCurrentIndex(value);
+                  },
+                ),
+                body: _pages[_currentIndex],
+              ));
+    });
   }
 }
